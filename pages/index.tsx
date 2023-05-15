cc2d452772540b99633621e839569771e5fc7e13
Code Queen <princessuhiene@gmail.com>
2023-05-15 20:51:32 +0000

    my first commit

---
+++ pages/index.tsx
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="p-4 max-w-md mx-auto">
+        <form className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">From Currency</label>
+            <select
+              id="fromCurrency"
+              name="fromCurrency"
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">To Currency</label>
+            <select
+              id="toCurrency"
+              name="toCurrency"
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+            </select>
+          </div>
+          <div>
+            <input
+              id="toCurrency"
+              name="toCurrency"
+              readOnly
+              placeholder='result'
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-green-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Convert
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
